1cdd3d061529c0083966bf261a904fc4c77ae4a8
Okuhle charlieman <okuhlecharlieman72@gmail.com>
2024-08-18 11:39:57 +0200

    responsiveness

--- app/(router)/_components/Header.jsx
+++ app/(router)/_components/Header.jsx
@@ -4,12 +4,12 @@ import { Button } from "@/components/ui/button";
 
 function Header() {
   return (
-    <div className="bg-white p-4 flex justify-between ">
+    <div className="bg-white p-4 md:flex justify-between ">
       <div className="flex gap-2 border p-2  rounded-full">
         <Search className="h-5 w-5" />
         <input className="outline-none" type="text" placeholder="Search..." />
       </div>
-      <div className="flex items-center gap-4">
+      <div className="flex items-center gap-4 mt-2">
         {" "}
         <BellDot className="" />
         <Button>Get stated</Button>
