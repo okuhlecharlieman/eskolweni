bac284bcf3be1e62e27e86b54d2de5b612e90418
Okuhle charlieman <okuhlecharlieman72@gmail.com>
2024-08-20 18:45:27 +0200

    error fixed :content display

--- app/(router)/course-preview/[courseId]/page.jsx
+++ app/(router)/course-preview/[courseId]/page.jsx
@@ -45,37 +45,39 @@ function CoursePreview({ params = { courseId: "clzwxkttu01ii08lb3v2o2nsn" } }) {
   return loading ? (
     <div>Loading...</div>
   ) : (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">{course.name}</h1>
-      <div className="mb-4">
-        <h2 className="text-xl font-semibold">Chapters:</h2>
-        <ul className="list-disc pl-5">
-          {course.chapters.map((chapter) => (
-            <li key={chapter.id} className="mb-2">
-              <div className="flex items-center justify-between">
-                <h3
-                  className="font-semibold cursor-pointer"
-                  onClick={() => toggleChapter(chapter.id)}
-                >
-                  {chapter.name}
-                </h3>
-                <button
-                  className="ml-2 text-blue-500"
-                  onClick={() => toggleChapter(chapter.id)}
-                >
-                  {expandedChapters[chapter.id] ? "-" : "+"}
-                </button>
-              </div>
-              {expandedChapters[chapter.id] && (
-                <div className="text-sm text-gray-600 md:text-base lg:text-lg xl:text-xl">
-                  {/* <div className="w-full md:w-3/4 lg:w-2/3 xl:w-1/2 h-48 md:h-64 lg:h-80 xl:h-96 "> */}
-                  <pre>{chapter.chapterContent}</pre>
-                  {/* </div> */}
+    <div className="p-5 m-5 rounded-lg bg-white">
+      <div className=" mb-4">
+        <h1 className="text-2xl font-bold mb-4">{course.name}</h1>
+        <div className="mb-4">
+          <h2 className="text-xl font-semibold">Chapters:</h2>
+          <ul className="list-disc pl-5">
+            {course.chapters.map((chapter) => (
+              <li key={chapter.id} className="mb-2">
+                <div className="flex items-center justify-between">
+                  <h3
+                    className="font-semibold cursor-pointer"
+                    onClick={() => toggleChapter(chapter.id)}
+                  >
+                    {chapter.name}
+                  </h3>
+                  <button
+                    className="ml-2 text-blue-500"
+                    onClick={() => toggleChapter(chapter.id)}
+                  >
+                    {expandedChapters[chapter.id] ? "-" : "+"}
+                  </button>
                 </div>
-              )}
-            </li>
-          ))}
-        </ul>
+                {expandedChapters[chapter.id] && (
+                  <div className="text-sm text-gray-600 md:text-base lg:text-lg xl:text-xl">
+                    {/* <div className="w-full md:w-3/4 lg:w-2/3 xl:w-1/2 h-48 md:h-64 lg:h-80 xl:h-96 "> */}
+                    <pre>{chapter.chapterContent}</pre>
+                    {/* </div> */}
+                  </div>
+                )}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
