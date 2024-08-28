de0d95ab6d20e3520d6a364a3027721dc990e9a6
Okuhle charlieman <okuhlecharlieman72@gmail.com>
2024-08-28 11:09:48 +0200

    added iframe

--- app/(router)/courses/_Components/Ads.jsx
+++ app/(router)/courses/_Components/Ads.jsx
@@ -1,41 +1,31 @@
-import React, { useEffect } from "react";
+import React from "react";
+import "./ads.css";
 
-function Ads() {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src =
-      "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5922051928401423";
-    script.async = true;
-    script.crossOrigin = "anonymous";
-    document.body.appendChild(script);
-
-    (window.adsbygoogle = window.adsbygoogle || []).push({});
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
+export default function Ads() {
+  // Array of video URLs
+  const videoUrls = [
+    "https://www.youtube.com/embed/WS09IRAai7c?si=rfGRcLtPbHJlME9a",
+    "https://www.youtube.com/embed/TEIcpEYGYtk?si=NSEljAKb2GF-oEag",
+    "https://www.youtube.com/embed/Fht_hT8Yuw8?si=BoEoxTEVlRMtEIXY",
+  ];
 
   return (
     <div className="p-5 mt-5 rounded-lg bg-white">
-      <div className="flex items-center justify-between mb-4">
-        {/* <h2 className="text-[20px] font-bold">Ads</h2> */}
-        <div style={{ width: "100%", minWidth: "200px", height: "320px" }}>
-          <ins
-            className="adsbygoogle"
-            style={{
-              display: "block",
-              width: "100%",
-              height: "100%",
-            }}
-            data-ad-format="autorelaxed"
-            data-ad-client="ca-pub-5922051928401423"
-            data-ad-slot="2031609536"
-          />
-        </div>
+      <div className="video-container mb-4">
+        {videoUrls.map((url, index) => (
+          <div className="video-wrapper mb-4" key={index}>
+            <iframe
+              className="border rounded-xl hover:shadow-md cursor-pointer hover:shadow-purple-600"
+              src={url}
+              title={`YouTube video player ${index + 1}`}
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
+          </div>
+        ))}
       </div>
     </div>
   );
 }
-
-export default Ads;
